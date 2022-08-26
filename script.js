b42eec9e099280a33388e642385929fbef5e0b9c
Trenton Dean <trentondean@icloud.com>
2022-08-25 22:27:13 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+var displayDiv = document.querySelector("#display");
+displayDiv.innerText = 0;
+var play = document.querySelector("audio");
+
+function press(e){ 
+
+    play.play();
+
+    if(displayDiv.innerText == 0){
+        displayDiv.innerText = e;
+    }else if(displayDiv.innerText != 0 && num1 == ""){
+        displayDiv.innerText += e;
+    }else if(operator != "" && displayDiv.innerText == num1){
+        displayDiv.innerText = e;
+    }else if(operator != "" && displayDiv.innerText != num1){
+        displayDiv.innerText += e;
+    }
+}
+
+function clr(){
+    displayDiv.innerText = 0;
+    operator = "";
+    num1 = "";
+    play.play();
+}
+
+var num1 = "";
+var num2 = "";
+var operator = "";
+
+function setOP(o){
+    num1 = displayDiv.innerText;
+    operator = o;
+    console.log(num1);
+    play.play();
+}
+
+var total = 0;
+
+function calculate(){
+    num2 = displayDiv.innerText;
+
+    play.play();
+    
+    switch (operator){
+        case '+':
+            total = num1 + num2;
+            displayDiv.innerText = total;
+            break;
+        case '-':
+            total = num1 - num2; 
+            displayDiv.innerText = total;
+            break;
+        case '*':
+            total = num1 * num2;
+            displayDiv.innerText = total;
+            break;
+        case '/':
+            total = num1 / num2;
+            displayDiv.innerText = total;
+    }
+}
